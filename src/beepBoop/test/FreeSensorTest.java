ae6b6c2d9bf8fcbd5df282e0bd63c18280ff514e
6runge <28950411+6runge@users.noreply.github.com>
2018-07-20 17:03:46 +0200

    [TASK] FreeSensorTest

---
+++ src/beepBoop/test/FreeSensorTest.java
@@ -0,0 +1,35 @@
+package beepBoop.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Point;
+
+import org.junit.Test;
+
+import beepBoop.model.FreeSensor;
+import beepBoop.model.Level;
+
+public class FreeSensorTest {
+	
+	@Test
+	public void check_freePosition_returnTrue(){
+		//arrange
+		FreeSensor freeSensor = new FreeSensor();
+		String directionToCheck = "L";
+		String[] params = new String[] {"someString", directionToCheck};
+		Point currentPosition = new Point(1,1);
+		Point freePosition = new Point(0,1);
+		@SuppressWarnings("serial")
+		Level mockLevel = new Level(null, null, null) {
+			@Override
+			public boolean isPositionFree(int x, int y) {
+				return x == freePosition.x && y == freePosition.y;
+			}
+		};
+		//act
+		boolean result = freeSensor.check(params, currentPosition, mockLevel);
+		//assert
+		assertTrue(result);
+	}
+
+}
