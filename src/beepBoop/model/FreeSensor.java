e3d4f6edc1e2ba6e03175f545f8b991c4dc85bed
6runge <28950411+6runge@users.noreply.github.com>
2018-08-08 19:37:32 +0200

    [TASK] comments for FreeSensor

--- src/beepBoop/model/FreeSensor.java
+++ src/beepBoop/model/FreeSensor.java
@@ -1,13 +1,16 @@
 package beepBoop.model;
 
 import java.awt.Point;
-
+/**
+ * A sensro that checks if the Tile next to the Robot in a given direction is free. 
+ * @author ptp18-d06(Pawel Rasch, Tim Runge)
+ *
+ */
 public class FreeSensor implements ISensor {
 	@Override
 	public boolean check(String[] params, Point p, Level level) {
 		Point target = Command.getPointFromDirection(p, params[1]);
 		return level.isPositionFree(target.x, target.y);
 	}
-
 
 }
