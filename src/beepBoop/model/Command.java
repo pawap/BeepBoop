00dd8289cf2a207dadc9042e63995676c6719e15
6runge <28950411+6runge@users.noreply.github.com>
2018-08-08 19:27:13 +0200

    [TASK] comments for Command

--- src/beepBoop/model/Command.java
+++ src/beepBoop/model/Command.java
@@ -2,12 +2,22 @@ package beepBoop.model;
 
 import java.awt.Point;
 
+/**
+ * Code in a Robot's memory is turned into Commands.
+ * Commands are processed by the RobotController.
+ * @author ptp18-d06(Pawel Rasch, Tim Runge)
+ *
+ */
 public class Command
 {
     
     private String type;
     private String[] args;
 
+    /**
+     * Constructor
+     * @param commandStr first the type, then the arguments; separated by spaces
+     */
     public Command(String commandStr)
     {
        if (commandStr.isEmpty()) {
@@ -23,26 +33,46 @@ public class Command
        }   
     }
 
+    /**
+     * @return the type of the Command
+     */
     public String getType()
     {
         return type;
     }
 
+    /**
+     * Set the type of the Command.
+     * @param type the desired type
+     */
     public void setType(String type)
     {
         this.type = type;
     }
 
+    /**
+     * @return the arguments of the Command
+     */
     public String[] getArgs()
     {
         return args;
     }
 
+    /**
+     * set the arguments of a Command
+     * @param args
+     */
     public void setArgs(String[] args)
     {
         this.args = args;
     }
 
+    /**
+     * Translates a given position into a new position, 1 tile away, based on a given direction
+     * @param position the origin position
+     * @param direction element in {"L", "R", "U", "D"}
+     * @return the new position 
+     */
     public static Point getPointFromDirection(Point position, String direction)
     {
         switch(direction) {
@@ -53,9 +83,5 @@ public class Command
             default: return position;          
         }
     }
-
-
-
-    
 
 }
