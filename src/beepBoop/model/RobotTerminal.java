716f58db08058758442571f420cacb9bafc63110
6runge <28950411+6runge@users.noreply.github.com>
2018-08-09 15:24:22 +0200

    [TASK] RobotTerminal comments

--- src/beepBoop/model/RobotTerminal.java
+++ src/beepBoop/model/RobotTerminal.java
@@ -1,9 +1,19 @@
 package beepBoop.model;
 
+/**
+ * The in-Landscape representation of the Robot Terminal. 
+ * The Player has to interact with this to access the RobotTerminal 
+ * Menu and thus the Robot Terminal functions.
+ * @author ptp18-d06(Pawel Rasch, Tim Runge)
+ *
+ */
 public class RobotTerminal extends Thing {
 	
 	private static final long serialVersionUID = 2497148692695664144L;
 
+	/**
+	 * Constructor
+	 */
 	public RobotTerminal() {
 		super(TileFactory.TERMINAL);
 	}
