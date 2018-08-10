dd292460ec768dd97df01a37bd4a5fa9421847b2
6runge <28950411+6runge@users.noreply.github.com>
2018-08-10 16:41:25 +0200

    [TASK] comments for and reorganizing of SensorService

--- src/beepBoop/service/SensorService.java
+++ src/beepBoop/service/SensorService.java
@@ -7,12 +7,17 @@ import beepBoop.model.FreeSensor;
 import beepBoop.model.ResourceSensor;
 import beepBoop.model.ISensor;
 
+/**
+ * Grants access to the robot sensors of BeepBoop.
+ * @author ptp18-d06(Pawel Rasch, Tim Runge)
+ *
+ */
 public class SensorService {
 
-	private static SensorService sensorService = new SensorService();
-	
+	private static SensorService sensorService;
 	private Map<String,ISensor> sensors;
 	
+	//private constructor defeats instantiation, sets up sensors for later access
 	private SensorService() {
 		super();
 		sensors = new HashMap<String,ISensor>();
@@ -20,11 +25,21 @@ public class SensorService {
 		sensors.put("RESOURCE", new ResourceSensor());		
 	}
 	
+	/**
+	 * @return the singleton instance of the SensorService
+	 */
 	public static SensorService getInstance() {
+		if (sensorService == null) {
+			sensorService = new SensorService();
+		}
 		return sensorService;
 	}
 
-	public ISensor getSensor(String name) {
-		return sensors.get(name);
+	/**
+	 * @param type element of {"FREE", "RESOURCE"}
+	 * @return a sensor of the requested type 
+	 */
+	public ISensor getSensor(String type) {
+		return sensors.get(type);
 	}
 }
