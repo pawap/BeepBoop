0a03fc57bde7eb32eb38b6c3c9cc836f2be304f0
6runge <28950411+6runge@users.noreply.github.com>
2018-08-10 15:50:53 +0200

    [TASK] EventController comments

--- src/beepBoop/controller/EventController.java
+++ src/beepBoop/controller/EventController.java
@@ -9,20 +9,46 @@ import beepBoop.model.MsgEvent;
 import beepBoop.service.EventQueue;
 import beepBoop.ui.MainFrame;
 
+/**
+ * Controller for BeepBoop Events
+ * @author ptp18-d06(Pawel Rasch, Tim Runge)
+ *
+ */
 public class EventController {
 
 	private Level level;
 	private MainFrame gui;
+	
+	/**
+	 * Constructor
+	 * @param level the current Level
+	 * @param gui the main gui
+	 */
+	public EventController(Level level, MainFrame gui) {
+		super();
+		this.level = level;
+		this.gui = gui;
+	}
+
+	/**
+	 * Have an Event happen.
+	 * @param event the Event that should happen
+	 */
 	public void processAction(Event event) {
 		event.performChanges(level);
 	}
 	
+	/**
+	 * Add an Event to the Level.
+	 * @param event
+	 */
 	public void addAction(Event event) {
 		level.addEvent(event);
 		prepareEvent(event);
 				
 	}
 	
+	//prepares an Event based on its type
 	private void prepareEvent(Event event) {
 		if (event instanceof MsgEvent) {
 			MsgEvent msgEvent = (MsgEvent) event;
@@ -36,16 +62,14 @@ public class EventController {
 		}
 	}
 	
+	/**
+	 * Prepares all Events in a given EventQueue
+	 * @param events the EventQueue containing the Events to be prepared
+	 */
 	public void initAction(EventQueue events) {
 		for (Event event: events) {
 			prepareEvent(event);
 		}
 	}
-	public EventController(Level level, MainFrame gui) {
-		super();
-		this.level = level;
-		this.gui = gui;
-	}
-
 	
 }
